822874bfb8d7a0f235714d291041852952ff3ad4
rwlc <yshsbhat@outlook.com>
2025-03-06 18:45:02 -0500

    fix: maybe

--- setup.sh
+++ setup.sh
@@ -51,9 +51,8 @@ fi
 echo -e "\n🐍 step 5/6: preparing python environment..."
 python3 -m venv "$INSTALL_DIR/venv" || { echo "failed to create python virtual environment. see log."; exit 1; }
 source "$INSTALL_DIR/venv/bin/activate"
-response=$(curl -s https://raw.githubusercontent.com/TrachSenseADC/trachhub/refs/heads/setup/requirements.txt)
-cat "$response"
-echo "$response" | pip install --no-cache-dir -q -r /dev/stdin
+response=$(curl -s https://raw.githubusercontent.com/TrachSenseADC/trachhub/refs/heads/setup/requirements.txt -o requirements.txt)
+pip install --no-cache-dir -q -r requirements.txt
 
 # launch trachub server in the background
 echo -e "\n🚀 step 6/6: starting trachub server..."
